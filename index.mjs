eceaeee5fb87019d2dd97c2705483c545131453a
Ryosuke Akiyama <r@554.jp>
2024-12-04 01:53:08 +0900

    Fix to allow installation via `npm install`
    - Remove unnecessary code.
    - Remove pre-build binaries.
    - Update dependencies.
    - Fix installation script.

--- index.mjs
+++ index.mjs
@@ -6,62 +6,8 @@ import { EventEmitter } from 'events';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-let AudioCapture;
-let moduleRoot = '';
-if (process.platform === 'darwin') {
-  moduleRoot = 'bin/darwin';
-} else if (process.platform === 'win32') {
-  moduleRoot = 'bin/win32';
-}
-AudioCapture = bindings({
-  bindings: 'addon',
-  module_root: path.join(__dirname, moduleRoot)
-}).AudioCapture;
+const AudioCapture = bindings('addon').AudioCapture;
 
 Object.setPrototypeOf(AudioCapture.prototype, EventEmitter.prototype);
 
 export { AudioCapture };
-
-
-async function showDesktopWindows() {
-  const [displays, windows] = await AudioCapture.enumerateDesktopWindows();
-  console.log("displays:");
-  displays.forEach(i => console.log(`[${i.displayId}]`));
-  console.log("windows:");
-  windows.forEach(i => console.log(`[${i.windowId}] ${i.title}`));
-}
-
-function captureDesktopAudio() {
-  const ws = fs.createWriteStream('test.raw');
-
-  const capture = new AudioCapture();
-
-  capture.on('error', (error) => {
-    console.error(error);
-  });
-  capture.on('data', (data) => {
-    ws.write(Buffer.from(data.buffer));
-  });
-
-  capture.startCapture({
-    channels: 1,
-    sampleRate: 16000,
-    displayId: 1,
-  });
-
-  let closed = false;
-  process.once('SIGINT', async () => {
-    if (closed) {
-      return;
-    }
-    await capture.stopCapture();
-    ws.close();
-    console.debug("exit");
-    closed = true;
-  });
-}
-
-console.log('start');
-showDesktopWindows();
-captureDesktopAudio();
-console.log('end');
--- install.js
+++ install.js
@@ -1,30 +1,34 @@
-import { exec } from 'child_process';
-import fs from 'fs';
+import { exec } from "node:child_process";
+import { exit } from "node:process";
 
 console.log(`running npm install for platform: ${process.platform}`);
-if (process.platform === 'darwin') {
-  exec('npx cmake-js compile', (error) => {
-    if (error) {
-      console.error(`Error in npm install: ${error}`);
-      return;
-    }
 
-    // TODO: MAC
-    // copy mac addon.node into ./bin/darwin/Release, and add addon.node to git.
+const command = [
+  "npx", "cmake-js", "compile",
+];
 
-    console.error(`TODO: darwin addon.node must be copied into ./bin/darwin/Release/ folder`);
-    //console.log('module installed successfully.');
-  });
-} else if (process.platform === 'win32') {
-  exec('npx cmake-js compile --O build/win32 --runtime=electron --runtime-version=30.1.0 --arch=x64', (error) => {
-    if (error) {
-      console.error(`Error in npm install: ${error}`);
-      return;
-    }
-    fs.mkdirSync('./bin/win32/Release', { recursive: true });
-    fs.copyFileSync('./build/win32/Release/addon.node', './bin/win32/Release/addon.node');
-    console.log('module installed successfully.');
-  });
-} else {
-  console.log(`unknown platform ${process.platform}`);
+switch (process.platform) {
+  case "darwin":
+    break;
+  case "win32":
+    command.push(
+      "--O", "build/win32",
+      "--runtime=electron",
+      "--runtime-version=30.1.0",
+      "--arch=x64",
+    );
+    break;
+  default:
+    console.error(`unsupported platform ${process.platform}`);
+    exit(1);
+    break;
 }
+
+exec(command.join(" "), (error) => {
+  if (error) {
+    console.error(`Error in npm install: ${error}`);
+    exit(1);
+    return;
+  }
+  console.log("module installed successfully.");
+});
