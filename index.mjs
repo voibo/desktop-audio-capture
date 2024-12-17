548bb72139437cf2171d8f7878dbc4d60f9fc3f7
Ryosuke Akiyama <r@554.jp>
2024-12-17 16:20:06 +0900

    Include native binaries for NodeJS Addon in the package

--- index.mjs
+++ index.mjs
@@ -6,7 +6,14 @@ import { EventEmitter } from 'events';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const AudioCapture = bindings('addon').AudioCapture;
+const AudioCapture = bindings({
+  bindings: 'addon',
+  try: [
+    ['module_root', 'build', 'Debug', 'bindings'],
+    ['module_root', 'build', 'Release', 'bindings'],
+    ['module_root', 'bin', 'platform', 'arch', 'bindings'],
+  ],
+}).AudioCapture;
 
 Object.setPrototypeOf(AudioCapture.prototype, EventEmitter.prototype);
 
--- scripts/postpack.js
+++
@@ -1,7 +0,0 @@
-import { rmSync } from "node:fs";
-
-function rm(path) {
-  rmSync(path, {force: true});
-}
-
-rm("./lib/libsamplerate/.npmignore");
