82e23b80890f22459d099b3cb81e5c336f4f2edd
Nobuhiro Hayashi <hayashi@humanest.jp>
2025-03-05 21:10:46 +0900

    MediaCapture: Implement platform-specific handling for MediaCapture on Apple Silicon
    Signed-off-by: Nobuhiro Hayashi <hayashi@humanest.jp>

--- index.mjs
+++ index.mjs
@@ -2,56 +2,98 @@ import { EventEmitter } from "events";
 import path from "path";
 import { fileURLToPath } from "url";
 import bindings from "bindings";
+import process from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// addon.node から AudioCapture と MediaCapture を取得
-const { AudioCapture, MediaCapture } = bindings("addon");
-
-// AudioCapture　はこれで動作しているので修正してはいけない
-Object.setPrototypeOf(AudioCapture.prototype, EventEmitter.prototype);
-export { AudioCapture };
-
-// EventEmitterの初期化方法を改善
-class EnhancedMediaCapture extends EventEmitter {
-  constructor() {
-    super();
-    this._nativeInstance = new MediaCapture();
-
-    // メソッドをこのクラスにバインド
-    this.startCapture = this._nativeInstance.startCapture.bind(
-      this._nativeInstance
-    );
-    this.stopCapture = this._nativeInstance.stopCapture.bind(
-      this._nativeInstance
-    );
-
-    // より堅牢なイベント転送の仕組み
-    const self = this;
-    this._nativeInstance.emit = function (event, ...args) {
-      // thisではなくselfにイベントを転送
-      return self.emit(event, ...args);
-    };
-
-    // emit関数の参照をC++側で正しく取得できるようにする
-    Object.defineProperty(this._nativeInstance, "_events", {
-      value: {},
-      writable: true,
-      enumerable: false,
-    });
-  }
-
-  // 必要に応じて静的メソッドを追加
-  static enumerateMediaCaptureTargets(...args) {
-    return MediaCapture.enumerateMediaCaptureTargets(...args);
-  }
-}
-export { EnhancedMediaCapture as MediaCapture };
-
-// エクスポートする品質設定の定数
+// Export quality setting constants
 export const MediaCaptureQuality = {
   High: 0,
   Medium: 1,
   Low: 2,
 };
+
+/// AudioCapture
+// AudioCapture is working as is, so do not modify
+const { AudioCapture } = bindings("addon");
+Object.setPrototypeOf(AudioCapture.prototype, EventEmitter.prototype);
+export { AudioCapture };
+
+/// MediaCapture
+// Currently only available on Apple Silicon environment
+const isAppleSilicon =
+  process.platform === "darwin" && process.arch === "arm64";
+let MediaCaptureImplementation;
+if (isAppleSilicon) {
+  // Only use the actual MediaCapture implementation on Apple Silicon
+  const { MediaCapture } = bindings("addon");
+
+  class EnhancedMediaCapture extends EventEmitter {
+    constructor() {
+      super();
+      this._nativeInstance = new MediaCapture();
+
+      // Bind methods to this class
+      this.startCapture = this._nativeInstance.startCapture.bind(
+        this._nativeInstance
+      );
+      this.stopCapture = this._nativeInstance.stopCapture.bind(
+        this._nativeInstance
+      );
+
+      // More robust event forwarding mechanism
+      const self = this;
+      this._nativeInstance.emit = function (event, ...args) {
+        // Forward events to self instead of this
+        return self.emit(event, ...args);
+      };
+
+      // Allow C++ side to correctly get reference to emit function
+      Object.defineProperty(this._nativeInstance, "_events", {
+        value: {},
+        writable: true,
+        enumerable: false,
+      });
+    }
+
+    // Add static methods as needed
+    static enumerateMediaCaptureTargets(...args) {
+      return MediaCapture.enumerateMediaCaptureTargets(...args);
+    }
+  }
+
+  MediaCaptureImplementation = EnhancedMediaCapture;
+} else {
+  // For unsupported environments, provide a dummy MediaCapture class
+  class UnsupportedMediaCapture extends EventEmitter {
+    constructor() {
+      super();
+      console.warn(
+        "MediaCapture is only available on Apple Silicon (ARM64) macOS devices."
+      );
+    }
+
+    startCapture() {
+      throw new Error(
+        "MediaCapture is not supported on this platform. Only available on Apple Silicon macOS."
+      );
+    }
+
+    stopCapture() {
+      throw new Error(
+        "MediaCapture is not supported on this platform. Only available on Apple Silicon macOS."
+      );
+    }
+
+    static enumerateMediaCaptureTargets() {
+      throw new Error(
+        "MediaCapture is not supported on this platform. Only available on Apple Silicon macOS."
+      );
+    }
+  }
+
+  MediaCaptureImplementation = UnsupportedMediaCapture;
+}
+
+export { MediaCaptureImplementation as MediaCapture };
